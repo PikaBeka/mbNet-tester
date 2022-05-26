13756e19885a702378b4aa0b4a04d32c06a5f105
PikaBeka <beka.kalmahanbet@gmail.com>
2022-05-26 17:42:03 +0600

    test on 10K and it looks like this

--- plot.py
+++ plot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-methods = ['array_naive', 'array_tiling',
-           'direct_global', 'direct_shared', 'unroll_cublass', 'unroll_global']
+methods = ['array_naive', 'array_tiling']
 labels = []
 times = []
+isNormalized = True
 
 for method in methods:
     height = []
@@ -18,15 +18,33 @@ for method in methods:
 
 w = 1/(len(methods)+1)  # width of the bar
 
-bar = np.arange(len(labels))  # places of the labels
+if not isNormalized:
+    bar = np.arange(len(labels))  # places of the labels
 
-for i in range(0, len(times)):
-    # print(str(len(bar)) + " " + str(len(times[i])))
-    plt.bar(bar + w * i, times[i], w, label=methods[i])  # plots abar on x axis
+    for i in range(0, len(times)):
+        # print(str(len(bar)) + " " + str(len(times[i])))
+        # plots abar on x axis
+        plt.bar(bar + w * i, times[i], w, label=methods[i])
 
-plt.xlabel("Configurations (IF_N, INPUT_S, OF_N)")  # xlabel
-plt.ylabel("Time(ms)")  # ylabel
-plt.title("Execution time of different configurations")  # title
-plt.xticks(bar + w * len(times)/2, labels, rotation='vertical')  # xtrics
-plt.legend()
-plt.show()
+    plt.xlabel("Configurations (IF_N, INPUT_S, OF_N)")  # xlabel
+    plt.ylabel("Time(ms)")  # ylabel
+    plt.title("Execution time of different configurations")  # title
+    plt.xticks(bar + w * len(times)/2, labels, rotation='vertical')  # xtrics
+    plt.legend()
+    plt.show()
+else:
+    bar = np.arange(len(labels))  # places of the labels
+
+    base = times[0].copy()
+
+    for i in range(0, len(times)):
+        for j in range(0, len(times[i])):
+            times[i][j] = base[j] / times[i][j]
+        plt.bar(bar + w * i, times[i], w, label=methods[i])
+
+    plt.xlabel("Configurations (IF_N, INPUT_S, OF_N)")  # xlabel
+    plt.ylabel("Time(ms)")  # ylabel
+    plt.title("Execution time of different configurations")  # title
+    plt.xticks(bar + w * len(times)/2, labels, rotation='vertical')  # xtrics
+    plt.legend()
+    plt.show()
